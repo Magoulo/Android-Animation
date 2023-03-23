dae4a997d3d4815aec391f781dd79bee26868360
Magoulo <83534780+Magoulo@users.noreply.github.com>
2023-03-23 11:54:07 +0100

    API Animation Screen elements created and linked to from mainActivity

---
+++ app/src/main/java/com/example/android_animation/ApiAnimationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android_animation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ApiAnimationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`activity_api_animation`)
+    }
+}
--- app/src/main/java/com/example/android_animation/MainActivity.kt
+++ app/src/main/java/com/example/android_animation/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.android_animation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 // This is where the android animation will be y'all
 class MainActivity : AppCompatActivity() {
+    private lateinit var apiAnimationBtn: Button
+    private lateinit var customAnimationBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        apiAnimationBtn = findViewById(R.id.apiAnimationBtn)
+        customAnimationBtn = findViewById(R.id.customAnimationBtn)
+
+        apiAnimationBtn.setOnClickListener{
+            startActivity(
+                Intent(this,
+                    ApiAnimationActivity::class.java)
+            )
+        }
+
+        customAnimationBtn.setOnClickListener{
+            startActivity(
+                Intent(this,
+                    ApiAnimationActivity::class.java)
+            )
+        }
     }
 }
