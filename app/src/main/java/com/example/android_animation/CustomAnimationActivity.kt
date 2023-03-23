b4deea50742ee5319781407a9a5f43f974e0b0ed
Magoulo <83534780+Magoulo@users.noreply.github.com>
2023-03-23 16:22:05 +0100

    Added functionality to the custom Animation button. The animation now starts and stops with a single button

--- app/src/main/java/com/example/android_animation/CustomAnimationActivity.kt
+++ app/src/main/java/com/example/android_animation/CustomAnimationActivity.kt
@@ -1,11 +1,34 @@
 package com.example.android_animation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.airbnb.lottie.LottieAnimationView
 
 class CustomAnimationActivity : AppCompatActivity() {
+
+    private lateinit var LottieAnimationView : LottieAnimationView
+    private lateinit var startCustomAnimationBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_custom_animation)
+
+        LottieAnimationView = findViewById(R.id.customAnimationView)
+        startCustomAnimationBtn = findViewById(R.id.customAnimationStartBtn)
+
+        var isPlaying = false;
+        startCustomAnimationBtn.setOnClickListener{
+
+            if(isPlaying){
+                LottieAnimationView.pauseAnimation()
+                startCustomAnimationBtn.text = "Start"
+            } else {
+                LottieAnimationView.playAnimation()
+                startCustomAnimationBtn.text = "Pause"
+            }
+            isPlaying = !isPlaying
+        }
     }
 }
