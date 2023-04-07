06c9fbb8234b8a14a5179a14efade9c6d017f2cc
Magoulo <83534780+Magoulo@users.noreply.github.com>
2023-04-07 21:05:09 +0200

    box is animated with scaling now but needs tweeking

--- app/src/main/java/com/example/android_animation/CustomAnimationActivity.kt
+++ app/src/main/java/com/example/android_animation/CustomAnimationActivity.kt
@@ -1,34 +1,37 @@
 package com.example.android_animation
-
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
-import com.airbnb.lottie.LottieAnimationView
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android_animation.R
 
 class CustomAnimationActivity : AppCompatActivity() {
 
-    private lateinit var LottieAnimationView : LottieAnimationView
+    private lateinit var imageView: ImageView
     private lateinit var startCustomAnimationBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_custom_animation)
 
-        LottieAnimationView = findViewById(R.id.customAnimationView)
+        imageView = findViewById(R.id.imageViewCustomAnimation)
         startCustomAnimationBtn = findViewById(R.id.customAnimationStartBtn)
 
-        var isPlaying = false;
-        startCustomAnimationBtn.setOnClickListener{
-
-            if(isPlaying){
-                LottieAnimationView.pauseAnimation()
-                startCustomAnimationBtn.text = "Start"
-            } else {
-                LottieAnimationView.playAnimation()
-                startCustomAnimationBtn.text = "Pause"
-            }
-            isPlaying = !isPlaying
+        startCustomAnimationBtn.setOnClickListener {
+            startAnimation()
         }
+    }
+
+    private fun startAnimation() {
+        val animation = AnimationUtils.loadAnimation(this, R.anim.tweenanimation)
+        imageView.startAnimation(animation)
+
+        // Start the color change animation
+        val colorAnimation = imageView.drawable as AnimationDrawable
+        imageView.setImageDrawable(colorAnimation)
+        colorAnimation.start()
     }
 }
