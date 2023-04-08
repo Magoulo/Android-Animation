d7ff919410f609803905bb67bcc6934754baef54
Magoulo <83534780+Magoulo@users.noreply.github.com>
2023-04-08 20:39:08 +0200

    The box animation is now smoother and the colors are interpolating between eachother.

--- app/src/main/java/com/example/android_animation/CustomAnimationActivity.kt
+++ app/src/main/java/com/example/android_animation/CustomAnimationActivity.kt
@@ -1,11 +1,20 @@
 package com.example.android_animation
+
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
+import android.graphics.PorterDuff
 import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
+import android.os.Handler
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.android_animation.R
 
 class CustomAnimationActivity : AppCompatActivity() {
@@ -29,9 +38,26 @@ class CustomAnimationActivity : AppCompatActivity() {
         val animation = AnimationUtils.loadAnimation(this, R.anim.tweenanimation)
         imageView.startAnimation(animation)
 
-        // Start the color change animation
-        val colorAnimation = imageView.drawable as AnimationDrawable
-        imageView.setImageDrawable(colorAnimation)
-        colorAnimation.start()
+        val colors = intArrayOf(
+            ContextCompat.getColor(this, R.color.blue),
+            ContextCompat.getColor(this, R.color.purple),
+            ContextCompat.getColor(this, R.color.fuchsia),
+            ContextCompat.getColor(this, R.color.yellow),
+            ContextCompat.getColor(this, R.color.green)
+        )
+
+        val animator = ValueAnimator.ofArgb(*colors)
+        animator.repeatCount = ValueAnimator.INFINITE
+        animator.repeatMode = ValueAnimator.REVERSE
+        animator.duration = 4000 // 4 seconds
+        animator.addUpdateListener {
+            val color = it.animatedValue as Int
+            imageView.setColorFilter(color)
+        }
+        animator.start()
     }
+
+
+
+
 }
