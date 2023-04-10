53f77918bb06e559bb479300f86a8cb20ffb89ef
Magoulo <83534780+Magoulo@users.noreply.github.com>
2023-04-10 11:03:46 +0200

    The animation and all parts within the animation is executing 15 times

--- app/src/main/java/com/example/android_animation/CustomAnimationActivity.kt
+++ app/src/main/java/com/example/android_animation/CustomAnimationActivity.kt
@@ -1,9 +1,6 @@
 package com.example.android_animation
 
-import android.animation.AnimatorSet
-import android.animation.ArgbEvaluator
-import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
+import android.animation.*
 import android.annotation.SuppressLint
 import android.graphics.PorterDuff
 import android.graphics.drawable.AnimationDrawable
@@ -15,6 +12,7 @@ import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
 import androidx.core.content.ContextCompat
 import com.example.android_animation.R
 
@@ -33,14 +31,39 @@ class CustomAnimationActivity : AppCompatActivity() {
         startCustomAnimationBtn = findViewById(R.id.customAnimationStartBtn)
 
         startCustomAnimationBtn.setOnClickListener {
-            startAnimation()
+
+                startAnimation()
+
         }
     }
 
     private fun startAnimation() {
-        val animation = AnimationUtils.loadAnimation(this, R.anim.tweenanimation)
-        imageViewRectangle.startAnimation(animation)
 
+        // The Box position and scaling animation
+        val customAnimation = AnimationUtils.loadAnimation(this, R.anim.tweenanimation)
+        var repeatCounter = 0
+        val maxRepeats = 28 // looping 15 times
+
+        customAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {
+            }
+
+            override fun onAnimationEnd(animation: Animation) {
+                // Increment the counter and restart the animation
+                if (repeatCounter < maxRepeats) {
+
+                    println("repeatCounter: $repeatCounter")
+                    imageViewRectangle.startAnimation(customAnimation)
+                }
+                repeatCounter += 1
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {
+            }
+        })
+        imageViewRectangle.startAnimation(customAnimation)
+
+        // The color animation
         val colors = intArrayOf(
             ContextCompat.getColor(this, R.color.blue),
             ContextCompat.getColor(this, R.color.purple),
@@ -48,16 +71,17 @@ class CustomAnimationActivity : AppCompatActivity() {
             ContextCompat.getColor(this, R.color.yellow),
             ContextCompat.getColor(this, R.color.green)
         )
-
-        val animator = ValueAnimator.ofArgb(*colors)
-        animator.repeatCount = ValueAnimator.INFINITE
-        animator.repeatMode = ValueAnimator.REVERSE
-        animator.duration = 4000 // 4 seconds
-        animator.addUpdateListener {
+        val colorAnimator = ValueAnimator.ofArgb(*colors)
+        colorAnimator.repeatCount = 14 // looping 5 times
+        colorAnimator.repeatMode = ValueAnimator.RESTART
+        colorAnimator.duration = 4000 // 4 seconds
+        colorAnimator.addUpdateListener {
             val color = it.animatedValue as Int
             imageViewRectangle.setColorFilter(color)
         }
+        colorAnimator.start()
 
+        // The opacity animation for the black line
         val animator1 = ValueAnimator.ofFloat(0.0f, 0.0f)
         animator1.duration = 960 // 24% of 4 seconds
         val animator2 = ValueAnimator.ofFloat(0.0f, 1.0f)
@@ -70,8 +94,6 @@ class CustomAnimationActivity : AppCompatActivity() {
         animator5.duration = 200 // 5% of 4 seconds
         val animator6 = ValueAnimator.ofFloat(1.0f, 1.0f)
         animator6.duration = 800 // 20% of 4 seconds
-        // val animator7 = ValueAnimator.ofFloat(0.0f, 0.0f)
-        //  animator7.duration = 0
 
         animator1.addUpdateListener {
             val alpha = it.animatedValue as Float
@@ -98,19 +120,19 @@ class CustomAnimationActivity : AppCompatActivity() {
             imageViewBlackLine.alpha = alpha
         }
 
-        val animatorSet = AnimatorSet()
-        animatorSet.playSequentially(
-            animator1,
-            animator2,
-            animator3,
-            animator4,
-            animator5,
-            animator6
-        ) // animator7
-        animatorSet.start()
+        val blackLineAnimatorSet = AnimatorSet()
+        val animationsList = mutableListOf<Animator>()
 
-        animator.start()
+        for (i in 0 until 15) {
+            animationsList.add(animator1.clone())
+            animationsList.add(animator2.clone())
+            animationsList.add(animator3.clone())
+            animationsList.add(animator4.clone())
+            animationsList.add(animator5.clone())
+            animationsList.add(animator6.clone())
+        }
+
+        blackLineAnimatorSet.playSequentially(animationsList) // animator7
+        blackLineAnimatorSet.start()
     }
-
-
 }
